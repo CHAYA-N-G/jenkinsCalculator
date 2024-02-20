1fdbce1b355e8f0fb30eea89d4b2926b02302ff4
Chaya <gangalchaya@gmail.com>
2024-02-20 09:57:35 +0530

    Updated the code changes related to square and cube operations

---
+++ Extended_Calculator.java
@@ -0,0 +1,23 @@
+class Extended_Calculator{
+    
+    /**
+     * The function "square" takes a float operand and returns the square of that operand.
+     * 
+     * @return The square of the operand_1 value.
+     */
+    float square(float operand_1){
+        return(operand_1*operand_1);
+    }
+
+    /**
+     * The function "cube" takes a number as input and returns the cube of that number.
+     * 
+     * @return The cube of the operand_1 value.
+     */
+    float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
+
+    
+     }
+
